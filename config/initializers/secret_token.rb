03d35008bd45b66920a3c9ca9e8b90438e0a5784
railsGH <vavalerian@live.fr>
2016-01-24 19:04:49 +0100

    Fin de l'application demo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '1f926643ca74160bdb642f02ab253f2b0c68b51989b27c711593c1965918d2e35532a5347dbb0b89096e8ba8d32a90e2c9b8de3c9bb8aebf5e06b4967cd22a8c'
+DemoApp::Application.config.secret_token = 'e37f353ca63b57d8e6992fa7a470a7f516f73d053901d16884796fab66b1a464e2f23d22777607f27b00806378776bf694de452ea194c3fe602af99ae55970c8'
